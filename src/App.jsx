25d600bd2f7f6bd9e9370d55cfe282752abf38fc
Volodymyr <volodymyrquo@gmail.com>
2021-01-24 08:08:51 +0200

    created primary structure of project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>this is app page...</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Musik.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Musik = () => {
+  return (
+    <div>
+      <h1>this is musik page...</h1>
+    </div>
+  );
+};
+
+export default Musik;
